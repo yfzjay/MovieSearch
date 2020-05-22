e63b53416958c87ab55a000a366b569ebb5b7e9b
yfzjay <yuanziang1997@qq.com>
2020-05-22 21:36:55 +0800

    first commit

--- movie/views.py
+++ movie/views.py
@@ -11,7 +11,7 @@ import redis
 
 client = Elasticsearch(hosts=["112.126.58.87"])
 
-pool = redis.ConnectionPool(host='127.0.0.1', port=6379, decode_responses=True)
+pool = redis.ConnectionPool(host='112.126.58.87', port=6379, decode_responses=True)
 redis_cli = redis.Redis(connection_pool=pool)
 
 response1 = client.search(
@@ -30,7 +30,8 @@ redis_cli.set("douban_count",response1['hits']['total']['value'])
 
 class TopView(View):
     def get(self,request):
-        topn_search = redis_cli.zrevrangebyscore("search_keywords_set_dou", "+inf", "-inf", start=0, num=5)
+        topn_search = redis_cli.zrevrangebyscore("search_keywords_set", "+inf", "-inf", start=0, num=5)
+        topn_search = [item.decode('utf8') for item in topn_search]
         return HttpResponse(json.dumps(topn_search), content_type="application/json")
 
 class SuggestView(View):
